bf2200232d9f4cefebac3885cd66e5e1952493e8
Fabio Kiatkowski <xolla@xolla.com.br>
2018-01-17 14:08:26 -0200

    Adiciona métodos auxliares para retorno dos estágios em aberto.

--- src/main/kotlin/br/com/pacificosul/data/ordens/EstagiosAProduzir200Data.kt
+++ src/main/kotlin/br/com/pacificosul/data/ordens/EstagiosAProduzir200Data.kt
@@ -3,5 +3,6 @@ package br.com.pacificosul.data.ordens
 data class EstagiosAProduzir200Data(val codigoEstagio: Int, val descricaoEstagio: String, val descricaoAgrupador: String,
                                     val apelido: String, val numeroRamal: Int, val quantidadePecasAProduzir: Number,
                                     val quantidadeOrdensAProduzir: Int) {
-    fun getEstagioComDescricao() = codigoEstagio.toString().padStart(2,'0')
+    fun getEstagioComDescricao() = codigoEstagio.toString().padStart(2,'0').plus("-").plus(descricaoEstagio)
+    fun getResponsavelEstagio() = apelido.plus("(").plus(numeroRamal).plus(")")
 }
