8e079c8d946b1cc50f8a22747083df54ecbb3e17
Fabio Kiatkowski <xolla@xolla.com.br>
2018-01-18 11:35:59 -0200

    Adiciona métodos auxliares para retorno dos estágios em aberto.

--- src/main/kotlin/br/com/pacificosul/api/ordens/PeriodoProducaoAbertoApi.kt
+++ src/main/kotlin/br/com/pacificosul/api/ordens/PeriodoProducaoAbertoApi.kt
@@ -6,6 +6,7 @@ import br.com.pacificosul.repository.ordens.PeriodoProducaoAbertoRepository
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = arrayOf("http://localhost:3000", "http://192.168.0.193:3000", "http://localhost:8080"))
 @RequestMapping("/api/periodo-producao")
 class PeriodoProducaoAbertoApi : DefaultApi() {
     @GetMapping("/teste")
--- src/main/kotlin/br/com/pacificosul/data/ordens/PeriodoProducaoAbertoData.kt
+++ src/main/kotlin/br/com/pacificosul/data/ordens/PeriodoProducaoAbertoData.kt
@@ -1,4 +1,6 @@
 package br.com.pacificosul.data.ordens
 
 data class PeriodoProducaoAbertoData(val codigoPeriodo: Int, val descricaoPeriodo: String, val quantidadePecas: Number,
-                                     val quantidadeOrdens: Int)
+                                     val quantidadeOrdens: Int) {
+    fun getPeriodoComDescricao() = codigoPeriodo.toString().plus(" - ").plus(descricaoPeriodo)
+}
