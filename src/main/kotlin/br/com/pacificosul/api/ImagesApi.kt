b0f4720c50f60399cfd70bee09d07c159051e1cf
paulorsouza <paulor1809@gmail.com>
2018-01-24 14:52:57 -0200

    Create images endpoints

--- src/main/kotlin/br/com/pacificosul/api/ImagesApi.kt
+++ src/main/kotlin/br/com/pacificosul/api/ImagesApi.kt
@@ -2,6 +2,7 @@ package br.com.pacificosul.api
 
 import br.com.pacificosul.rules.Images
 import br.com.pacificosul.data.Image
+import org.springframework.http.MediaType
 import java.io.File;
 import java.io.FileInputStream;
 import org.springframework.web.bind.annotation.*
@@ -10,52 +11,55 @@ import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RequestMapping
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.io.IOException
+import org.springframework.web.bind.annotation.GetMapping
+
+
 
 @RestController
+@CrossOrigin(origins = arrayOf("http://localhost:3000", "http://192.168.0.193:3000", "http://localhost:8080"))
 @RequestMapping("/api/images")
 class ImagesApi {
 
-    val DIR_BASE = "/home/prs/Pictures/"
+    val DIR_FOTOS_PRODUTOS = "/mnt/servidor/fotos/"
+    val DIR_FOTOS_INSUMOS = "/mnt/servidor/produc/fotos/insumos/"
 
-    @PostMapping("/upload")
-    fun post(@RequestBody image: Image): String {
-        val path = DIR_BASE + image.name
-        Images.toImage(image.data.orEmpty(), path)
-        return "WIP - !"
+//    @GetMapping("produto")
+//    @ResponseBody
+//    fun get(@RequestParam("nivel") nivel: String,
+//            @RequestParam("grupo") grupo: String,
+//            @RequestParam("subGrupo") subGrupo: String,
+//            @RequestParam("item") item: String): Image? {
+//        val imagePath = findPath(nivel, grupo, subGrupo, item)
+//        val name = Images.pathDestructor(imagePath)
+//        return Image(name.orEmpty(), imagePath)
+//    }
+//
+    @GetMapping("/produto/referencia/{referencia}")
+    @ResponseBody
+    fun get(@PathVariable("referencia") referencia: String): HashMap<String, List<Image>> {
+        var ref = referencia
+        if (referencia.first().isLetter()) {
+            ref = referencia.replaceRange(0,1,"2")
+        }
+        return Images.getImages(DIR_FOTOS_PRODUTOS, ref)
     }
 
-    @GetMapping("/download/produtoConfeccionado/referencia/{referencia}")
-    @ResponseBody
-    fun get(@PathVariable("referencia") referencia: String,
-            @RequestParam("tipo", required = false) tipo: String?,
-            @RequestParam("item", required = false) item: String?,
-            @RequestParam( "height", required = false) height: Int?,
-            @RequestParam("subGrupo", required = false) subGrupo: String?): List<Image> {
-        val paths: List<String> = listPaths(referencia);
-        val images = arrayListOf<Image>()
-        paths.forEach({ path ->
-            println(path)
-            images.add(getBase64(path, height))
-        })
-        return images
-    }
+//    @GetMapping("/download/produto")
+//    @ResponseBody
+//    fun get(@RequestParam("nivel") nivel: String,
+//            @RequestParam("grupo") grupo: String,
+//            @RequestParam("subGrupo") subGrupo: String,
+//            @RequestParam("item") item: String,
+//            @RequestParam( "height", required = false) height: Int?): Image? {
+//        val imagePath = findPath(nivel, grupo, subGrupo, item);
+//        return getBase64(imagePath, height)
+//    }
 
-    @GetMapping("/download/produto")
+    @GetMapping("/base64/download")
     @ResponseBody
-    fun get(@RequestParam("nivel") nivel: String,
-            @RequestParam("grupo") grupo: String,
-            @RequestParam("subGrupo") subGrupo: String,
-            @RequestParam("item") item: String,
-            @RequestParam( "height", required = false) height: Int?): Image? {
-        val imagePath = findPath(nivel, grupo, subGrupo, item);
-        return getBase64(imagePath, height)
-    }
-
-    @GetMapping("/downloadByFileName")
-    @ResponseBody
-    fun get(@RequestParam("fileName") fileName: String,
-            @RequestParam( "height", required = false) height: Int?): Image? {
-        val imagePath = DIR_BASE + fileName
+    fun get(@RequestParam("imagePath") imagePath: String,
+            @RequestParam( "height", required = false) height: Int?): String {
         val file = File(imagePath)
         var bytes: ByteArray
         if (height != null) {
@@ -66,9 +70,15 @@ class ImagesApi {
 
         val imageBase64 = Images.toBase64(bytes)
 
-        return if (imageBase64 != null) {
-            Image(fileName, imageBase64)
-        } else null
+        return imageBase64.orEmpty()
+    }
+
+    @GetMapping(value = "/download", produces = arrayOf(MediaType.IMAGE_JPEG_VALUE))
+    @ResponseBody
+    @Throws(IOException::class)
+    fun getImage(@RequestParam("imagePath") imagePath: String): ByteArray {
+        val file = File(imagePath)
+        return file.readBytes()
     }
 
     fun getBase64(path: String, height: Int?): Image {
@@ -83,16 +93,7 @@ class ImagesApi {
         return Image(file.nameWithoutExtension, base64.orEmpty())
     }
 
-    fun findPath(nivel: String, grupo: String, subGrupo: String, item: String): String {
-        return DIR_BASE + "Screenshot_20180102_105952.jpeg"
-    }
-
-    fun listPaths(referencia: String) : List<String> {
-        val paths = arrayListOf<String>()
-        val dir = Paths.get("/home/prs/Pictures/")
-        File("/home/prs/Pictures/").walkTopDown().forEach({
-            x -> if(x.isFile && !x.isHidden) paths.add(x.toString())
-        })
-        return paths;
-    }
+//    fun findPath(nivel: String, grupo: String, subGrupo: String, item: String): String {
+//        return DIR_BASE + "Screenshot_20180102_105952.jpeg"
+//    }
 }
--- src/main/kotlin/br/com/pacificosul/api/ordens/OrdensPendentesNoEstagiosApi.kt
+++ src/main/kotlin/br/com/pacificosul/api/ordens/OrdensPendentesNoEstagiosApi.kt
@@ -8,6 +8,7 @@ import br.com.pacificosul.repository.ordens.PeriodoProducaoAbertoRepository
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = arrayOf("http://localhost:3000", "http://192.168.0.193:3000", "http://localhost:8080"))
 @RequestMapping("/api/ordem-pendente-estagio")
 class OrdensPendentesNoEstagiosApi : DefaultApi() {
     @GetMapping("/periodo-estagio")
