14282bb13959babf90d596530d3029bdc2ac3cc2
Paulo Ricardo de Souza <paulor1809@gmail.com>
2018-03-02 11:38:24 -0300

    Create conf_grid_perfil_column table

--- script_sql/conf_grid_columns.sql
+++ script_sql/conf_grid_columns.sql
@@ -1,20 +1,22 @@
 create table pacificosul.conf_grid_column(
     id number(9),
     id_grid number(9),
-    name_key varchar2(50) not null,
+    key varchar2(50) not null,
+    name varchar2(50) not null,
     type varchar2(50) not null,
-    formatter number(3) default 0
+    formatter number(3) default 0,
 )
 
-COMMENT ON TABLE pacificosul.conf_grid_column IS 'Cadastro de colunas das grids do orion web';
-COMMENT ON COLUMN pacificosul.conf_grid_column.name_key IS 'Chave da coluna para linkar o response do backend com a coluna no frontend';
+COMMENT ON TABLE pacificosul.conf_grid_column IS 'Cadastro de colunas das grids do orion web, nomes dos campos de configuração estão em ingles para ficar coerente com o frontend';
+COMMENT ON COLUMN pacificosul.conf_grid_column.key IS 'Chave da coluna para linkar o response do backend com a coluna no frontend';
+COMMENT ON COLUMN pacificosul.conf_grid_column.name IS 'Descrição da coluna';
 COMMENT ON COLUMN pacificosul.conf_grid_column.type IS 'Tipo de dados para o frontend: text, number, date';
 COMMENT ON COLUMN pacificosul.conf_grid_column.formatter IS 'Tipo de formatação da coluna no frontend: 0 - Sem formatação, 1 - IntegerFormat';
 
 ALTER TABLE pacificosul.conf_grid_column ADD CONSTRAINT pk_conf_grid_column PRIMARY KEY (id);
 ALTER TABLE pacificosul.conf_grid_column
     ADD CONSTRAINT REF_CONF_GRID_CONF_GRID_COLUMN FOREIGN KEY(ID_GRID) REFERENCES PACIFICOSUL.CONF_GRID(ID);
-ALTER TABLE pacificosul.conf_grid_column ADD CONSTRAINT UNIQ_CONF_GRID_COLUMN UNIQUE(id_grid, name_key);
+ALTER TABLE pacificosul.conf_grid_column ADD CONSTRAINT UNIQ_CONF_GRID_COLUMN UNIQUE(id_grid, key);
 
 CREATE SEQUENCE pacificosul.id_conf_grid_column START WITH 1 INCREMENT BY 1;
 
